3d15653381bc7c6b0e21d98b5e584a42ec8c612f
theo-commits <me@tlindsey.cloud>
2021-09-01 18:34:47 -0500

    golang script

--- go/main.go
+++ go/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	wget := exec.Command("wget", "https://raw.githubusercontent.com/Project-Reclass/toynet-react/master/docker-compose.yml")
+	log.Printf("Starting Toynet....")
+	wgetErr := wget.Run()
+	if wgetErr != nil {
+		log.Printf("Toynet Container failed to run with error: %v", wgetErr)
+	}
+
+	toynet := exec.Command("docker-compose", "-f", "docker-compose.yml", "up", "-d", "--build")
+	log.Printf("Starting Toynet....")
+	toynetErr := toynet.Run()
+	if toynetErr != nil {
+		log.Printf("Toynet Container failed to run with error: %v", toynetErr)
+	}
 }
